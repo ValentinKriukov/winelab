7f31720e712ef7a6f0accbb06e2b27b13a204c6c
Valentin <valentin-krukov@mail.ru>
2021-07-22 19:57:54 +0300

    Добавлен футер

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -3,8 +3,16 @@ import "./footer.css";
 
 const FooterBox=()=>{
     return(
-        <div className='footer'>footer
-        </div>  
+        <div className="footer">
+            <div className="footerBox">
+                 <div>ВИННЫЙ БУТИК LE CORTE</div>
+                 <div>4 8-555-333-222</div>
+                 <div>иконки</div>
+                 <div>@LE-CORTE.RU</div>
+            </div>
+        
+        </div>
+    
     )
     
 };
